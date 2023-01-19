b10e4da932a3367ff2d316e4a3a076f884c19b04
hasanchamdany <hasanchamdany@gmail.com>
2023-01-19 20:28:41 +0700

    feat(tailwind): add tailwind and config files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,98 @@
+import Link from "next/link";
+import { useState } from "react";
+import Image from "next/image";
+
+const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+  return (
+    <>
+      <div className="flex justify-center mx-10  ">
+        <nav className="fixed z-[9999999] mt-4 mx-4 w-full xl:max-w-[1850px] lg:max-w-[1400px] h-[50px] bg-white shadow-xl xl:rounded-[50px] rounded-[20px] drop-shadow-2xl">
+          <div className="justify-center pt-3 px-4 mx-auto xl:max-w-[1850px] lg:max-w-[1400px] md:items-center md:flex md:px-8">
+            <div>
+              <div className="flex items-center justify-between py-1 md:py-2 md:block">
+                
+                <div className="md:hidden">
+                  <button
+                    className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {navbar ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6 text-n-blue"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6 text-n-blue"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    )}
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div
+                className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                  navbar ? "block" : "hidden"
+                }`}
+              >
+                <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                  <li className="text-n-blue">
+                    <Link href="/wisata-mengwi">
+                      <div className="font-poppins font-semibold ">
+                        Home
+                      </div>
+                    </Link>
+                  </li>
+                  <li className="text-n-blue">
+                    <Link href="/komoditas-mengwi">
+                      <div className="font-poppins font-semibold ">
+                        Portofolio
+                      </div>
+                    </Link>
+                  </li>
+                  <li className="text-n-blue">
+                    <Link href="/tentang-mengwi">
+                      <div className="font-poppins font-semibold ">
+                        Project
+                      </div>
+                    </Link>
+                  </li>
+                  <li className="text-n-blue">
+                    <Link href="/umkm-mengwi">
+                      <div className="font-poppins font-semibold ">
+                       Contect Me
+                      </div>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      // Natural: ['Natural', 'serif'],
+      // poppins: ['Poppins', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        current: "currentColor",
+        // 's-orange': '#E16A3D',
+        
+      },
+      backgroundImage: {
+        // 'home-1':"url('/images/background/home1.png')",
+        'landing-1':"url('/images/bg-landing-page.png')"
+      }
+    },
+  },
+  plugins: [],
+}
