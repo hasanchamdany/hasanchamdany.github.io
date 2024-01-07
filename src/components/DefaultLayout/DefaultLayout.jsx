8ce34926ae657745e07108f8671b7a4df39dd7b4
hasanchamdany <hasanchamdany@gmail.com>
2024-01-07 08:12:07 +0700

    feat(header): add header icon

--- src/components/DefaultLayout/DefaultLayout.jsx
+++ src/components/DefaultLayout/DefaultLayout.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Head from 'next/head'
 import Navbar from '../Navbar/Navbar.jsx';
 import Footer from '../Footer/Footer.jsx';
-
+// import HeaderImage from '@images/header,png'
 
 export default function DefaultLayout({ title = 'hasanchamdany', children }) {
 	return (
 		<div className="relative">
 			<Head>
 				<title>{title}</title>
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="./images/header.png" />
 			</Head>
             <div className="w-fit max-w-full overflow-x-hidden">
 			<Navbar />
