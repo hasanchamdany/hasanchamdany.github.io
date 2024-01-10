8253fe7d25d71fc5ed5bae67b38bb41fecc4b0c9
hasanchamdany <hasanchamdany@gmail.com>
2024-01-10 18:08:10 +0700

    fix(role-card): fix padding and image width

--- src/components/Card/RoleCard.jsx
+++ src/components/Card/RoleCard.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const RoleCard = ({image, text}) => {
   return (
     <div className="rounded-[30px] role-card">
-      <div className="p-8">
-        <Image src={image} width={270} />
+      <div className="p-14">
+        <Image src={image} width={200} />
       </div>
       <div className="flex justify-center pt-4 pb-8">
         <h3 className="font-bold text-white text-2xl">{text}</h3>
