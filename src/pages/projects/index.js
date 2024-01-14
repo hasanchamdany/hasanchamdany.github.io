b418dba63f6d88229e8b4c65f08226a333ee9b25
hasanchamdany <hasanchamdany@gmail.com>
2024-01-14 07:48:45 +0700

    experiment(decap-cms): experimental cms decap

---
+++ src/pages/projects/index.js
@@ -0,0 +1,14 @@
+import DefaultLayout from '@/components/DefaultLayout/DefaultLayout'
+import React from 'react'
+
+const index = () => {
+  return (
+    <DefaultLayout>
+      <div className='w-full h-screen bg-dark-base'>
+
+      </div>
+    </DefaultLayout>
+  )
+}
+
+export default index
